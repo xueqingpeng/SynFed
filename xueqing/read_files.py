e5f4ec17b7b46750a8ba0619f9c039784d43563f
xueqing_misha <xueqing.peng@yale.edu>
2025-07-09 22:47:28 -0400

    baseline_success

--- xueqing/read_files.py
+++ xueqing/read_files.py
@@ -47,20 +47,23 @@ def main():
         df = pd.DataFrame(list(data))[["syn_pmt_id", "status", "gain_score", "status_score"]]
         df_success = df[df["status"] == "SUCCESS"]
 
-        df_success_grouped = df_success.groupby("syn_pmt_id")
-        df_success_grouped_top = df_success_grouped.apply(lambda x: x.nlargest(int(len(x) * 0.2), 'gain_score'))
-        df_success_grouped_bottom = df_success_grouped.apply(lambda x: x.nsmallest(int(len(x) * 0.2), 'gain_score'))
-        print(f"Top 20% gain scores range: {df_success_grouped_top['gain_score'].min()} - {df_success_grouped_top['gain_score'].max()}. ({len(df_success_grouped_top)}/{len(df_success)} rows selected.)")
-        print(f"Bottom 20% gain scores range: {df_success_grouped_bottom['gain_score'].min()} - {df_success_grouped_bottom['gain_score'].max()}. ({len(df_success_grouped_bottom)}/{len(df_success)} rows selected.)")
+        # df_success_grouped = df_success.groupby("syn_pmt_id")
+        # df_success_grouped_top = df_success_grouped.apply(lambda x: x.nlargest(int(len(x) * 0.2), 'gain_score'))
+        # df_success_grouped_bottom = df_success_grouped.apply(lambda x: x.nsmallest(int(len(x) * 0.2), 'gain_score'))
+        print(f"SUCCESS gain scores range: {df_success['gain_score'].min()} - {df_success['gain_score'].max()}. ({len(df_success)}/{len(df)} rows selected.)")
+        # print(f"Top 20% gain scores range: {df_success_grouped_top['gain_score'].min()} - {df_success_grouped_top['gain_score'].max()}. ({len(df_success_grouped_top)}/{len(df_success)} rows selected.)")
+        # print(f"Bottom 20% gain scores range: {df_success_grouped_bottom['gain_score'].min()} - {df_success_grouped_bottom['gain_score'].max()}. ({len(df_success_grouped_bottom)}/{len(df_success)} rows selected.)")
         
         fp_sft = f"/gpfs/radev/home/xp83/Documents/project/scripts/SynFed/syned_datasets/{round}_sft_fed/{data_name}_client_0.train.jsonl"
-        fp_sft_top = f"/gpfs/radev/home/xp83/Documents/project/scripts/SynFed/syned_datasets/{round}_sft_fed/{data_name}_client_0_top_20.train.jsonl"
-        fp_sft_bottom = f"/gpfs/radev/home/xp83/Documents/project/scripts/SynFed/syned_datasets/{round}_sft_fed/{data_name}_client_0_bottom_20.train.jsonl"
+        fp_sft_success = f"/gpfs/radev/home/xp83/Documents/project/scripts/SynFed/syned_datasets/{round}_sft_fed/{data_name}_client_0_success.train.jsonl"
+        # fp_sft_top = f"/gpfs/radev/home/xp83/Documents/project/scripts/SynFed/syned_datasets/{round}_sft_fed/{data_name}_client_0_top_20.train.jsonl"
+        # fp_sft_bottom = f"/gpfs/radev/home/xp83/Documents/project/scripts/SynFed/syned_datasets/{round}_sft_fed/{data_name}_client_0_bottom_20.train.jsonl"
 
         data_sft = read_jsonl_files(fp_sft)
         df_sft = pd.DataFrame(list(data_sft))
-        df_sft.iloc[df_success_grouped_top.index.get_level_values(1),:].to_json(fp_sft_top, orient="records", lines=True)
-        df_sft.iloc[df_success_grouped_bottom.index.get_level_values(1),:].to_json(fp_sft_bottom, orient="records", lines=True)
+        df_sft.iloc[df_success.index,:].to_json(fp_sft_success, orient="records", lines=True)
+        # df_sft.iloc[df_success_grouped_top.index.get_level_values(1),:].to_json(fp_sft_top, orient="records", lines=True)
+        # df_sft.iloc[df_success_grouped_bottom.index.get_level_values(1),:].to_json(fp_sft_bottom, orient="records", lines=True)
 
         print(f"Done {data_name}!")
 
@@ -81,6 +84,7 @@ if __name__ == "__main__":
 # 80% quantile: 0.0
 # 20% quantile: -0.5163977794943222
 # 0% quantile: -0.8355491589367869
+# SUCCESS gain scores range: -0.8355491589367869 - 0.5163977794943223. (6078/9038 rows selected.)
 # Top 20% gain scores range: 0.0 - 0.5163977794943223. (1215/6078 rows selected.)
 # Bottom 20% gain scores range: -0.8355491589367869 - -0.5163977794943222. (1215/6078 rows selected.)
 # Positive gain scores range: 0.1197655832620661 - 0.5163977794943223. (1069/6078 rows selected.)
@@ -91,6 +95,7 @@ if __name__ == "__main__":
 # 80% quantile: -0.31915137944246474
 # 20% quantile: -0.5773502691896258
 # 0% quantile: -0.8355491589367869
+# SUCCESS gain scores range: -0.8355491589367869 - 0.19724640005185756. (404/6728 rows selected.)
 # Top 20% gain scores range: -0.31915137944246474 - 0.19724640005185756. (80/404 rows selected.)
 # Bottom 20% gain scores range: -0.8355491589367869 - -0.5773502691896258. (80/404 rows selected.)
 # Positive gain scores range: 0.19724640005185756 - 0.19724640005185756. (6/404 rows selected.)
@@ -101,6 +106,7 @@ if __name__ == "__main__":
 # 80% quantile: 0.5163977794943222
 # 20% quantile: 0.2581988897471611
 # 0% quantile: -0.31915137944246474
+# SUCCESS gain scores range: -0.31915137944246474 - 0.8355491589367869. (847/6032 rows selected.)
 # Top 20% gain scores range: 0.5163977794943222 - 0.8355491589367869. (169/847 rows selected.)
 # Bottom 20% gain scores range: -0.31915137944246474 - 0.2581988897471611. (169/847 rows selected.)
 # Positive gain scores range: 0.2581988897471611 - 0.8355491589367869. (684/847 rows selected.)
